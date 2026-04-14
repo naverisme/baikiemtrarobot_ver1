d44f50181c7020483a38404e00c075698b2a2a3d
mrvuu205 <128308388+mrvuu205@users.noreply.github.com>
2026-04-14 23:39:46 +0700

    ver2

---
+++ mentor/ikine_Mentor.m
@@ -0,0 +1,44 @@
+function [q_Up, q_Down] = ikine_Mentor(Px, Py, Pz, phi_pitch_deg, theta5_roll_deg)
+
+    phi_user = deg2rad(phi_pitch_deg);
+    phi_math = pi/2 - phi_user; 
+    th5 = deg2rad(theta5_roll_deg);
+
+   d1 = 30; a2 = 20; a3 = 15; d5 = 10;
+
+    th1 = atan2(Py, Px);
+
+    R = sqrt(Px^2 + Py^2);
+    Rw = R - d5 * sin(phi_math);
+    Zw_prime = Pz - d1 - d5 * cos(phi_math);
+
+    D2 = Rw^2 + Zw_prime^2;
+    c3 = (D2 - a2^2 - a3^2) / (2 * a2 * a3);
+
+    if c3 < -1 || c3 > 1
+        error('Toa do nam ngoai tam voi!');
+    end
+
+    s3_up = sqrt(1 - c3^2);
+    th3_std_up = atan2(s3_up, c3);
+    th3_std_down = atan2(-s3_up, c3);
+
+    alpha = atan2(Zw_prime, Rw);
+    
+    beta_up = atan2(a3 * s3_up, a2 + a3 * c3);
+    th2_std_up = alpha - beta_up;
+    
+    beta_down = atan2(a3 * (-s3_up), a2 + a3 * c3);
+    th2_std_down = alpha - beta_down;
+
+    th2_up = -th2_std_up;
+    th3_up = -th3_std_up;
+    th2_down = -th2_std_down;
+    th3_down = -th3_std_down;
+
+    th4_up = phi_math - th2_up - th3_up;
+    th4_down = phi_math - th2_down - th3_down;
+
+    q_Up = [th1, th2_up, th3_up, th4_up, th5];
+    q_Down = [th1, th2_down, th3_down, th4_down, th5];
+end
---
+++ mentor/main_mentor.m
@@ -0,0 +1,53 @@
+
+L(1) = Link([0,  30, 0,  -pi/2, 0]);
+L(2) = Link([0,  0,  20, 0,     0]);
+L(3) = Link([0,  0,  15, 0,     0]);
+L(4) = Link([0,  0,  0,  pi/2,  0]);
+L(5) = Link([0,  10, 0,  0,     0]);
+mentor = SerialLink(L, 'name', 'Mentor_5_DOF');
+
+disp('--- CHON QUY DAO MUON VE ---');
+disp('1. Duong thang 3D');
+disp('2. Duong tron mat phang');
+disp('3. Hinh so 8');
+choice = input('Nhap lua chon: ');
+n = 100;
+
+switch choice
+    case 1
+        toado = veduongthang([25, 10, 20], [25, -10, 30], n);
+        title_str = "Quy dao Duong Thang";
+    case 2
+        toado = vehinhtron([25, 0, 25], 8, n);
+        title_str = "Quy dao Duong Tron";
+    case 3
+        toado = vehinhso8([25, 0, 25], 5, n);
+        title_str = "Quy dao Hinh so 8";
+    otherwise
+        error('Lua chon khong hop le!');
+end
+
+matrix_q = zeros(n, 5);
+pitch = 0; 
+roll = 0;
+
+for i = 1:n
+    [q1, q2] = ikine_Mentor(toado(i,1), toado(i,2), toado(i,3), pitch, roll);
+    matrix_q(i, :) = q1; 
+end
+
+figure('Name', title_str, 'Position', [100, 100, 800, 600]);
+plot3(toado(:,1), toado(:,2), toado(:,3), 'r--', 'LineWidth', 1.5);
+hold on; grid on; axis equal; view(135, 30);
+
+xlim([-60 60]); ylim([-60 60]); zlim([-10 70]);
+
+mentor.plot(matrix_q(1,:), 'noshadow');
+
+for i = 1:n
+    mentor.animate(matrix_q(i, :));
+    plot3(toado(i,1), toado(i,2), toado(i,3), 'b*', 'MarkerSize', 6);
+    drawnow;
+    pause(0.03);
+end
+disp('Mo phong hoan tat!');
